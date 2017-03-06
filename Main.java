0ec8299873c5bc75c575f322233e05826b1aa0fe
Danny Gonzalez <gdanny120@gmail.com>
2017-03-05 18:15:59 -0800

    Update Main

--- Main.java
+++ Main.java
@@ -1,36 +1,31 @@
 package com.company;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+
+
+
+
     CardBackPack CBP = new CardBackPack();
 
-
-    //Fills out the card deck with card whose question & answer are the i
-    for(int i = 0;i<=100;i++){
-        String s = Integer.toString(i);
-        Card temp = new Card(s,s);
-        CBP.AddCard(temp);
-    }
-
-
-    for(int i = 0; i<CBP.Deck.size(); i++){
-        CBP.CreateQuiz();
-        System.out.println("[Q#"+(i+1)+"]####################################[]");
-        System.out.println("[Q]:"+CBP.CurrentQuiz.Question);
-        System.out.println("[1]:"+CBP.CurrentQuiz.Answer1);
-        System.out.println("[2]:"+CBP.CurrentQuiz.Answer2);
-        System.out.println("[3]:"+CBP.CurrentQuiz.Answer3);
-        System.out.println("[4]:"+CBP.CurrentQuiz.Answer4);
-        System.out.println("[A#"+(i+1)+"]####################################[]");
-        System.out.println("Please enter your Answer: ");
-        String j = scanner.next();
-        int k = Integer.parseInt(j);
-        CBP.CheckAnswer(k);
-    }
+    Card d = new Card("Who made this?","Danny");
+    CBP.AddCard(d);
+    d = new Card("How many bits does a short have?","16 bits");
+    CBP.AddCard(d);
+    d = new Card("How many bits does a int have?","32 bits");
+    CBP.AddCard(d);
+    d = new Card("How many bits does a char have?","16 bits");
+    CBP.AddCard(d);
+    d = new Card("How many bits does a double have?","64 bits");
+    CBP.AddCard(d);
+    CBP.CreateQuiz();
+    Controller c = new Controller(CBP);
 
 
 
